871a2aad7fb06e064bade3f8f0f4c7a373095a22
pguardia-dam <pguardia.dam@institutcampalans.net>
2025-11-11 19:51:13 +0100

    holasdadsada

---
+++ provaGit/provaGit/adeuJulia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provaGit
+{
+    internal class adeuJulia
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adeu Julia!");
+        }
+    }
+}
