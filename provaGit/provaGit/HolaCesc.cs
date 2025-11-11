82fc4d414ae9b5b228c3d0d9c10d4687553b9b38
pguardia-dam <pguardia.dam@institutcampalans.net>
2025-11-11 19:36:19 +0100

    merge

---
+++ provaGit/provaGit/HolaCesc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provaGit
+{
+    internal class HolaCesc
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Cesc!");
+        }
+    }
+}
